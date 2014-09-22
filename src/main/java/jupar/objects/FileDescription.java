6b75c18f5a5244801d17d71fefcf4716631651e3
Osama Khalid <samkool@gmail.com>
2014-09-22 17:54:24 +0500

    adding FileDescription class

---
+++ src/main/java/jupar/objects/FileDescription.java
@@ -0,0 +1,27 @@
+package jupar.objects;
+
+public class FileDescription {
+
+    private String destination;
+    private String filename;
+    private String hash;
+    
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getHash() {
+		return hash;
+	}
+	public void setHash(String hash) {
+		this.hash = hash;
+	}
+}
